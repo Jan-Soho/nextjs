2344fe4f26173ac29248e502a898f1267823bc64
Jan-Soho <nima.janlou@gmail.com>
2021-05-16 16:51:30 +0200

    Switch user

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,5 +2,6 @@ import "../styles/globals.css";
 import { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
+  // Yes
   return <Component {...pageProps} />;
 }
